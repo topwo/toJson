bba0061c768078d1cee5702af8513402b0ff2ac0
topwo <topwo@foxmail.com>
2017-12-28 23:37:26 +0800

    一个excel输出json的小程序

---
+++ ToJson/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ToJson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabJson = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tabJson.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Excel文件|*.xls;*.xlsx";
+            this.openFileDialog1.RestoreDirectory = true;
+            // 
+            // tabJson
+            // 
+            this.tabJson.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabJson.Controls.Add(this.tabPage1);
+            this.tabJson.Controls.Add(this.tabPage2);
+            this.tabJson.Location = new System.Drawing.Point(0, 37);
+            this.tabJson.Name = "tabJson";
+            this.tabJson.SelectedIndex = 0;
+            this.tabJson.Size = new System.Drawing.Size(802, 443);
+            this.tabJson.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(794, 417);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "Excel";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(788, 411);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(794, 417);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Json";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(788, 411);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "All types|*.*";
+            this.saveFileDialog1.RestoreDirectory = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(414, 8);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(122, 20);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(541, 6);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "解析";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(351, 10);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 16);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "*.xls";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(620, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 25);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "生成";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(304, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(723, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 25);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "保存";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 480);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabJson);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(818, 518);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel转JSON [http://www.topwo.com]";
+            this.tabJson.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TabControl tabJson;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ToJson/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace ToJson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Enabled = false;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = openFileDialog1.FileName;
+                    comboBox1.DataSource = GetExcelSheets(openFileDialog1.FileName);
+                    comboBox1.DisplayMember = "TABLE_NAME";
+                    comboBox1.ValueMember = "TABLE_NAME";
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                   /* if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        // Insert code to read the stream here.
+                        myStream.Close();
+                    }*/
+                }
+                this.Enabled = true;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Enabled = true;
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Enabled = false;
+                InitData();
+                this.Enabled = true;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Enabled = false;
+
+                JsonSerializer serializer = JsonSerializer.Create(new JsonSerializerSettings()
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+                serializer.Converters.Add(new DataTableConverter());
+                StringWriter sw = new StringWriter();
+                serializer.Serialize(new JsonTextWriter(sw), dataGridView1.DataSource);
+
+                ParseJson(sw.ToString());
+
+
+                this.Enabled = true;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                this.Enabled = false;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    //获得文件路径
+                    //localFilePath = saveFileDialog1.FileName.ToString();
+
+                    //获取文件名，不带路径
+                    //fileNameExt = localFilePath.Substring(localFilePath.LastIndexOf("\\") + 1);
+
+                    //获取文件路径，不带文件名
+                    //FilePath = localFilePath.Substring(0, localFilePath.LastIndexOf("\\"));
+
+                    //给文件名前加上时间
+                    //newFileName = DateTime.Now.ToString("yyyyMMdd") + fileNameExt;
+
+                    //在文件名里加字符
+                    //saveFileDialog1.FileName.Insert(1,"dameng");
+
+                    StreamWriter sw = new StreamWriter(saveFileDialog1.FileName, false, Encoding.UTF8);
+                    sw.Write(richTextBox1.Text);
+                    sw.Close();
+                    sw.Dispose();
+                    System.Diagnostics.Process.Start(saveFileDialog1.FileName.Substring(0, saveFileDialog1.FileName.LastIndexOf("\\")));
+                }
+                this.Enabled = true;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Enabled = true;
+            }
+        }
+
+        private void InitData()
+        {
+            DataSet ds = GetExcelToDataTableBySheet(textBox1.Text, comboBox1.Text);
+            dataGridView1.DataSource = ds.Tables[0];
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                dataGridView1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+        }
+        public  DataSet GetExcelToDataTableBySheet(string FileFullPath, string SheetName)
+        {
+            string strConn = GetConStr(FileFullPath);
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            OleDbDataAdapter odda = new OleDbDataAdapter(string.Format("SELECT * FROM [{0}]", SheetName), conn);
+            DataSet ds = new DataSet();
+            odda.Fill(ds, SheetName);
+            conn.Close();
+            return ds;
+        }
+        public  DataTable GetExcelSheets(string FileFullPath)
+        {
+            string strConn = GetConStr(FileFullPath);
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            // 得到包含数据架构的数据表
+            DataTable dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            conn.Close();
+            return dt;
+        }
+        public DataTable FormatTable(DataTable data_table_src)
+        {
+            DataTable data_table_dest = new DataTable("Format_" + data_table_src.TableName); //dt.Clone();
+            foreach (DataColumn dcVal in data_table_src.Columns)
+            {
+                DataColumn dc;
+                if (!dcVal.ColumnName.Contains("'"))
+                {
+                    if (dcVal.ColumnName.Contains("."))
+                    {
+                        dc = new DataColumn(dcVal.ColumnName, typeof(float));
+                    }
+                    else if (dcVal.ColumnName.Contains("!"))
+                    {
+                        dc = new DataColumn(dcVal.ColumnName, System.Type.GetType("System.Boolean"));
+                    }
+                    else
+                    {
+                        dc = new DataColumn(dcVal.ColumnName, typeof(int));
+                    }
+                }
+                else
+                {
+                    dc = new DataColumn(dcVal.ColumnName);//System.Type.GetType("System.Boolean")
+                }
+                data_table_dest.Columns.Add(dc);
+            }
+            foreach (DataRow drVal in data_table_src.Rows)
+            {
+                //向B中增加行
+                data_table_dest.ImportRow(drVal);    //表结构（列类型）不同的话将复制失败，但不会有异常
+            }
+            return data_table_dest;
+        }
+        public void ParseJson(string json_string)
+        {
+            if (json_string.Contains("[") && json_string.Contains("]"))
+            {
+                var ja = JArray.Parse(json_string);
+                richTextBox1.Text = ja.ToString();
+            }
+            else
+            {
+                var jo = JObject.Parse(json_string);
+                richTextBox1.Text = jo.ToString();
+            }
+            if (richTextBox1.TextLength > 0)
+            {
+                button4.Enabled = true;
+            }
+        }
+        public string GetConStr(string FileFullPath)
+        {
+            string strConn = "Provider=Microsoft.Ace.OleDb.12.0;" + "data source=" + FileFullPath + ";Extended Properties='Excel 12.0; HDR=YES; IMEX=1'"; //此連接可以操作.xls與.xlsx文件HDR=YES第一行为列名
+            if (checkBox1.Checked)
+            {
+                strConn = "Provider=Microsoft.Jet.OleDb.4.0;" + "data source=" + FileFullPath + ";Extended Properties='Excel 8.0; HDR=YES; IMEX=1'"; //此連接只能操作Excel2007之前(.xls)文件
+            }
+            return strConn;
+        }
+    }
+}
